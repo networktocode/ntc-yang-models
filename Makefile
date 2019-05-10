4f91a3968fba823d771f8a3309f4bda7e1d80dc8
David Barroso <dbarrosop@dravetech.com>
2019-05-10 14:12:17 +0200

    progress

--- Makefile
+++ Makefile
@@ -1,9 +1,64 @@
 BASE=models/
+OC_PYANG_PLUGINS_DIR="/src/oc-pyang/openconfig_pyang/plugins"
+
+PYANG=docker run --rm -v $(PWD):/ntc-yang-models oc-pyang pyang
+PYTEST=docker run --rm -v $(PWD):/ntc-yang-models yangson pytest
+YANGSON=docker run --rm -v $(PWD):/ntc-yang-models yangson yangson
+
+.PHONY: build-oc-pyang-container
+build-oc-pyang-container:
+	docker build -t oc-pyang -f Dockerfile.oc-pyang .
+
+.PHONY: build-yangson-container
+build-yangson-container:
+	docker build -t yangson -f Dockerfile.yangson .
 
 .PHONY: tree
 tree:
-	yangson -p $(shell find models/* -type d | xargs | sed 's/ /:/g') -t models/ntc-models-library.json
+	$(YANGSON) -p $(shell find models/* -type d | xargs | sed 's/ /:/g') -t models/ntc-models-library.json
 
 .PHONY: tests
 tests:
-	pytest
+	$(PYTEST)
+
+.PHONY: docs
+docs:
+	$(PYANG) \
+		--plugindir $(OC_PYANG_PLUGINS_DIR) \
+		-f docs \
+		--path models \
+		--doc-format html \
+		--doc-title="NTC YANG models" \
+		models/arp/*.yang > docs/arp.html
+	$(PYANG) \
+		--plugindir $(OC_PYANG_PLUGINS_DIR) \
+		-f docs \
+		--path models \
+		--doc-format html \
+		--doc-title="NTC YANG models" \
+		models/system/*.yang > docs/system.html
+	$(PYANG) \
+		--plugindir $(OC_PYANG_PLUGINS_DIR) \
+		-f docs \
+		--path models \
+		--doc-format html \
+		--doc-title="NTC YANG models" \
+		models/vlan/*.yang > docs/vlan.html
+	$(PYANG) \
+		--plugindir $(OC_PYANG_PLUGINS_DIR) \
+		-f docs \
+		--path models \
+		--doc-format html \
+		--doc-title="NTC YANG models" \
+		models/vrf/*.yang > docs/vrf.html
+
+.PHONY: lint
+lint:
+	$(PYANG) \
+		--plugindir $(OC_PYANG_PLUGINS_DIR) \
+		--path models \
+		--doc-format html \
+		--doc-title="NTC YANG models" \
+		--allowed-prefixes=ntc \
+		--openconfig \
+		models/*/*.yang
---
+++ docs/js/yangdoc.menu.js
@@ -0,0 +1,86 @@
+// Modifications copyright 2015 Google, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// MIT License
+//
+// Original work Copyright (c) 2015 Sean Wessell
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+//
+// Original work may be obtained from bootsnipp.com
+// http://bootsnipp.com/SeanWessell/snippets/ypNAe
+
+$.fn.extend({
+    treed: function (o) {
+
+      var openedClass = 'glyphicon-minus-sign';
+      var closedClass = 'glyphicon-plus-sign';
+
+      if (typeof o != 'undefined'){
+        if (typeof o.openedClass != 'undefined'){
+        openedClass = o.openedClass;
+        }
+        if (typeof o.closedClass != 'undefined'){
+        closedClass = o.closedClass;
+        }
+      };
+
+    //initialize each of the top levels
+    var tree = $(this);
+    tree.addClass("tree");
+    tree.find('li').has('ul').each(function () {
+        var branch = $(this); //li with children ul
+        branch.prepend("<i class='indicator glyphicon " + closedClass + "'></i>");
+        branch.addClass('branch');
+        branch.on('click', function (e) {
+            if (this == e.target) {
+                var icon = $(this).children('i:first');
+                icon.toggleClass(openedClass + " " + closedClass);
+                $(this).children().children().toggle();
+            }
+        })
+        branch.children().children().toggle();
+    });
+    //fire event from the dynamically added icon
+  tree.find('.branch .indicator').each(function(){
+    $(this).on('click', function () {
+        $(this).closest('li').click();
+    });
+  });
+
+    tree.find('.branch>button').each(function () {
+        $(this).on('click', function (e) {
+            $(this).closest('li').click();
+            e.preventDefault();
+        });
+    });
+}
+});
